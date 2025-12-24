90985e9e685f32cbeb5fe6c884b6d2af45c5ad1c
dynamiteMOJO <krtksng@gmail.com>
2025-12-24 12:43:57 +0000

    feat: Add initial implementation for textile attributes and item types

---
+++ kniterp/kniterp/doctype/textile_attribute_value/test_textile_attribute_value.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Kartik and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestItemAttributeValue(FrappeTestCase):
+	pass
