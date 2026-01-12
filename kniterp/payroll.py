8b7a1b1ff090329da01597e8b929092eaa11f440
dynamiteMOJO <krtksng@gmail.com>
2026-01-12 15:21:08 +0000

    removed print cmds

--- kniterp/payroll.py
+++ kniterp/payroll.py
@@ -5,8 +5,6 @@ my_logger = frappe.logger("payroll_custom_logs")
 
 MACHINE_EXTRA_RATE = 150
 
-print("debug logs")
-print("info logss")
 
 def calculate_variable_pay(slip, method):
     employee = slip.employee
@@ -14,27 +12,33 @@ def calculate_variable_pay(slip, method):
     end = slip.end_date
 
     per_day_salary = get_per_day_salary(slip)
-    
-    # reset components
-    set_component(slip, "Dual Shift Pay", 0)
-    set_component(slip, "Machine Extra Pay", 0)
-    set_component(slip, "Conveyance Allowance", 0)
-    set_component(slip, "Absent / Rejected Day Deduction", 0)
+    tea = get_variable_pay(employee, start)
 
+    # reset components
+    # set_component(slip, "Dual Shift Pay", 0)
+    # set_component(slip, "Machine Extra Pay", 0)
+    # set_component(slip, "Conveyance Allowance", 0)
+    # set_component(slip, "Absent / Rejected Day Deduction", 0)
+
+    sunday_pays = get_sunday_pay(employee, start, end)
     dual_shift_days = get_dual_shift_days(employee, start, end)
     machine_extra_pay = get_machine_extra_pay(employee, start, end)
-    conveyance = get_conveyance(employee, start)
+    conveyance = get_conveyance(employee, start, end)
     rejected_days = get_rejected_holiday_days(employee, start, end)
-
+    tea_pay = (slip.payment_days - rejected_days) * (tea / slip.total_working_days)
+    set_component(slip, "Sunday Pay", sunday_pays * per_day_salary)
     set_component(slip, "Dual Shift Pay", dual_shift_days * per_day_salary)
     set_component(slip, "Machine Extra Pay", machine_extra_pay)
     set_component(slip, "Conveyance Allowance", conveyance)
     set_component(
         slip,
-        "Absent / Rejected Day Deduction",
+        "Rejected Holiday Deduction",
         rejected_days * per_day_salary
     )
-    # slip.calculate_totals()
+    print("tea_pay:", tea_pay)
+    print("per_day_salary:", per_day_salary)
+    set_component(slip, "Tea Allowance", tea_pay)
+
     slip.calculate_net_pay()
     
 
@@ -58,7 +62,7 @@ def get_per_day_salary(slip):
     )
 
     base = ssa if ssa else 0
-    tea = get_component_amount(slip, "Tea Allowance")
+    tea = get_variable_pay(slip.employee, slip.start_date)
       
 
     # HRMS already calculates total_working_days correctly
@@ -67,6 +71,34 @@ def get_per_day_salary(slip):
 
     return int((base + tea) / slip.total_working_days)
 
+
+def get_variable_pay(employee, start):
+    tea = frappe.db.sql("""
+        SELECT variable
+        FROM `tabSalary Structure Assignment`
+        WHERE employee=%s
+        AND from_date <= %s
+        ORDER BY from_date DESC
+        LIMIT 1
+    """, (employee, start), as_dict=True)
+
+    return tea[0]['variable'] if tea[0]['variable'] else 0
+
+def get_sunday_pay(employee, start, end):
+    rows = frappe.db.sql("""
+        SELECT distinct attendance_date
+        FROM `tabAttendance`
+        WHERE employee=%s
+        AND attendance_date BETWEEN %s AND %s
+        AND status='Present'
+    """, (employee, start, end), as_dict=True)
+
+    sunday_count = 0
+    for r in rows:
+        if getdate(r.attendance_date).weekday() == 6:
+            sunday_count += 1
+
+    return sunday_count
 
 def get_dual_shift_days(employee, start, end):
     rows = frappe.db.sql("""
@@ -101,16 +133,15 @@ def get_machine_extra_pay(employee, start, end):
     return total_extra
 
 
-def get_conveyance(employee, month_start):
-    return frappe.db.get_value(
-        "Monthly Conveyance",
-        {
-            "employee": employee,
-            "month": month_start
-        },
-        "amount"
-    ) or 0
+def get_conveyance(employee, month_start, month_end):
+    amount  = frappe.db.sql("""
+        SELECT SUM(amount) as amount
+        FROM `tabMonthly Conveyance`
+        WHERE employee=%s
+        AND month BETWEEN %s AND %s
+    """, (employee, month_start, month_end), as_dict=True)
 
+    return amount[0]['amount'] if amount[0]['amount'] is not None else 0
 
 def get_rejected_holiday_days(employee, start, end):
     rejected = 0
@@ -122,22 +153,36 @@ def get_rejected_holiday_days(employee, start, end):
     )
 
     for h in holidays:
+        if is_present(employee, h):
+            continue
+
         prev_day = add_days(h, -1)
         next_day = add_days(h, 1)
 
-        if is_leave(employee, prev_day) and is_leave(employee, next_day):
+        if is_absent(employee, prev_day) and is_absent(employee, next_day):
             rejected += 1
 
     return rejected
 
 
-def is_leave(employee, date):
+def is_present(employee, date):
     return frappe.db.exists(
         "Attendance",
         {
             "employee": employee,
             "attendance_date": date,
-            "status": "On Leave"
+            "status": "Present"
+        }
+    )
+
+
+def is_absent(employee, date):
+    return frappe.db.exists(
+        "Attendance",
+        {
+            "employee": employee,
+            "attendance_date": date,
+            "status": "Absent"
         }
     )
 
