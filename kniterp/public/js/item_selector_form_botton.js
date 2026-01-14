e48f7d21879aa5e0d028e89bad9b5ab1b1054a4b
dynamiteMOJO <krtksng@gmail.com>
2026-01-14 14:23:03 +0000

    made item selector form button modular

--- kniterp/public/js/item_selector_form_botton.js
+++ kniterp/public/js/item_selector_form_botton.js
@@ -1,51 +1,45 @@
+window.kniterp_attach_item_selector = function (opts) {
+    const {
+        frm,
+        label = "Select Item by Attributes",
+        child_table = "items",
+        item_field = "item_code"
+    } = opts;
+
+    frm.add_custom_button(label, () => {
+        kniterp_open_item_selector({
+            on_select(item_code) {
+                let row = frm.doc[child_table]?.find(r => !r[item_field]);
+
+                if (!row) {
+                    row = frm.add_child(child_table);
+                }
+
+                frappe.model.set_value(
+                    row.doctype,
+                    row.name,
+                    item_field,
+                    item_code
+                );
+            }
+        });
+    });
+};
+
 frappe.ui.form.on("Sales Order", {
     refresh(frm) {
-        frm.add_custom_button(
-            "Select Item by Attributes",
-            () => {
-                kniterp_open_item_selector({
-                    on_select(item_code) {
-                        let row = frm.doc.items.find(r => !r.item_code);
-
-                        if (!row) {
-                            row = frm.add_child("items");
-                        }
-
-                        frappe.model.set_value(
-                            row.doctype,
-                            row.name,
-                            "item_code",
-                            item_code
-                        );
-                    }
-                });
-            }
-        );
+        kniterp_attach_item_selector({ frm });
     }
 });
 
 frappe.ui.form.on("Purchase Order", {
     refresh(frm) {
-        frm.add_custom_button(
-            "Select Item by Attributes",
-            () => {
-                kniterp_open_item_selector({
-                    on_select(item_code) {
-                        let row = frm.doc.items.find(r => !r.item_code);
+        kniterp_attach_item_selector({ frm });
+    }
+});
 
-                        if (!row) {
-                            row = frm.add_child("items");
-                        }
-
-                        frappe.model.set_value(
-                            row.doctype,
-                            row.name,
-                            "item_code",
-                            item_code
-                        );
-                    }
-                });
-            }
-        );
+frappe.ui.form.on("Delivery Note", {
+    refresh(frm) {
+        kniterp_attach_item_selector({ frm });
     }
 });
